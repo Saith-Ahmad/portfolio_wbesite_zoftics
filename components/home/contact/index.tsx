5fff968a63d350dcc03e226d7c8f601698e76e6a
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-10-26 07:09:56 +0500

    update

--- components/home/contact/index.tsx
+++ components/home/contact/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { motion } from "framer-motion";
 import { BorderBeam } from "@/components/ui/border-beam";
 import React from "react";
@@ -6,20 +6,24 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="container py-12 px-6 my-12 w-full" id="contact">
-      
       <motion.div
-      initial={{ opacity: 0, y: -65 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.9, delay: 0.25 }}
-      className="flex flex-col justify-center items-center gap-3">
-        <div className="text-primary active relative">We are eager to help you<BorderBeam size={70} duration={5}/> </div>
-        <h2 className="heading-2 text-white text-center mb-8 font-medium">Lets get in Touch!</h2>
+        initial={{ opacity: 0, y: -65 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.9, delay: 0.25 }}
+        className="flex flex-col justify-center items-center gap-3"
+      >
+        <div className="text-primary active relative">
+          We are eager to help you
+          <BorderBeam size={70} duration={20} />{" "}
+        </div>
+        <h2 className="heading-2 text-white text-center mb-8 font-medium">
+          Lets get in Touch!
+        </h2>
       </motion.div>
 
       <form className="bg-black bg-opacity-50 backdrop-blur-lg border-1 border-primary box_shadow p-6 rounded-3xl shadow-md mx-auto w-full relative overflow-hidden z-[10]">
-        <BorderBeam size={300} duration={15}/>
+        <BorderBeam size={300} duration={100} />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
-          
           <div>
             <label htmlFor="name" className="block text-white mb-2">
               Name
@@ -58,10 +62,7 @@ const Contact = () => {
         </div>
 
         <div className="text-center">
-          <button
-            type="submit"
-            className="btn_primary"
-          >
+          <button type="submit" className="btn_primary">
             Send Message
           </button>
         </div>
--- components/home/services/index.tsx
+++ components/home/services/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { motion } from "framer-motion";
 import React from "react";
 import Card from "../card";
@@ -8,21 +8,38 @@ import Marketing from "../../../app/assets/marketing.png";
 import { BorderBeam } from "@/components/ui/border-beam";
 
 const Services = () => {
-
   return (
     <div className="container mx-auto py-12 px-4 my-20" id="services">
       <motion.div
-         initial={{ opacity: 0, y: -65 }}
-         whileInView={{ opacity: 1, y: 0 }}
-         transition={{ duration: 0.9, delay: 0.25 }}
-      className="flex flex-col justify-center items-center gap-3">
-        <div className="text-primary active relative">Find the best services in the town <BorderBeam size={70} duration={4}/></div>
-        <h2 className="heading-2 text-white text-center mb-8 font-medium">The best Solutions For Your Business!</h2>
+        initial={{ opacity: 0, y: -65 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.9, delay: 0.25 }}
+        className="flex flex-col justify-center items-center gap-3"
+      >
+        <div className="text-primary active relative">
+          Find the best services in the town{" "}
+          <BorderBeam size={70} duration={20} />
+        </div>
+        <h2 className="heading-2 text-white text-center mb-8 font-medium">
+          The best Solutions For Your Business!
+        </h2>
       </motion.div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <Card image={Web} description="Custom websites tailored to your business needs." serviceName="Web Development"/>
-        <Card image={Seo} description="Boost your website's visibility on search engines." serviceName="SEO Optimization"/>
-        <Card image={Marketing} description="Effective strategies to grow your business online." serviceName="Digital Marketing"/>
+        <Card
+          image={Web}
+          description="Custom websites tailored to your business needs."
+          serviceName="Web Development"
+        />
+        <Card
+          image={Seo}
+          description="Boost your website's visibility on search engines."
+          serviceName="SEO Optimization"
+        />
+        <Card
+          image={Marketing}
+          description="Effective strategies to grow your business online."
+          serviceName="Digital Marketing"
+        />
       </div>
     </div>
   );
